017ac9d5a9628adf83271d33036eadad2e6f58cd
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-04-19 17:59:12 +0530

    css not completed ghost want to add perfect

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,19 @@ const Hero = () => {
         <source src="/videos/bgvideo.mp4" type="video/mp4" />
       </video>
       <img id="gost" src="/images/gost-removebg-preview.png" alt="Ghost" />
-      <div className='flickering-light1'></div>
+      <div className='flickering-light1'>
+      </div>
+      <img src="/images/ghost.png" alt="ghost" 
+        style={{
+          zIndex: -1,
+          width: '250px',
+          position: 'absolute',
+          top: '29%',
+          left: '80%',
+          transform: 'translate(-50%, -50%)',
+          overflow: 'hidden',
+        }}
+      />
       <div className='flickering-light2'></div>
       <div className='flickering-light3'></div>
       <div className="content">
